936815caf2bcccefd058d582a5d5ec42b156197b
Hay <huskyr@gmail.com>
2010-09-23 01:18:20 +0200

    Trying to write some javascript

--- plugin/class-like-dont-like.php
+++ plugin/class-like-dont-like.php
@@ -4,5 +4,26 @@ class LikeDontLike extends HaybasePlugin {
     
     function __construct() {
         parent::__construct(self::NAME);
+        $this->init();
+    }
+    
+    public function init() {
+        add_filter('the_content', array($this, "addButtons"));
+    }
+    
+    public function addButtons() {
+    ?>
+        <div class="likedontlike">
+            <div class="ldl-results">
+                <p class="ldl-result-like">12 People liked this</p>
+                <p class="ldl-result-dont-like">5 People didn't like this</p>
+            </div>
+            
+            <div class="ldl-buttons">
+                <button class="ldl-button-like">LIKE</button>
+                <button class="ldl-button-dont-like">DON'T LIKE</button>
+            </div>
+        </div>
+    <?php
     }
 }
